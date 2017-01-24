b0ca0a8b4bbcd8151cf59d6df08bf9ee5255f6e4
goodaddict <areaklik@gmail.com>
2017-01-24 08:56:10 +0700

    Update safelinkv2.js

--- safelinkv2.js
+++ safelinkv2.js
@@ -23,7 +23,7 @@ var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
 if(x=="1"){xxx="why-wont-some-videos-play-in-windows-81.html"}
 if(x=="2"){xxx="intel-skylake-prepare-for-faster-pcs.html"}
-if(x=="3"){xxx="watch-out-for-software-with-fake"}
+if(x=="3"){xxx="watch-out-for-software-with-fake.html"}
 if(x=="4"){xxx="microsoft-may-have-sneaked-windows-10.html"}a_to_ve[i].href="http://linkdownnontontvseries.blogspot.com/2015/09/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
